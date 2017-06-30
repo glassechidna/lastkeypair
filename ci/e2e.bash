cd4c5b3d8ee2325261f740d0b1fa8af60f2fa125
Aidan Steele <aidan.steele@glassechidna.com.au>
2017-06-30 16:56:52 +1000

    run openssh client in docker too

--- ci/e2e.bash
+++ ci/e2e.bash
@@ -6,7 +6,6 @@ export PATH=$PATH:$HOME/.local/bin # for awscli
 S3_BUCKET=lkp-lambda-test
 S3_KEY=handler.zip
 
-make # build lambda package
 aws s3 cp handler.zip s3://$S3_BUCKET/$S3_KEY
 S3_OBJVER=$(aws s3api head-object --bucket $S3_BUCKET --key $S3_KEY --query VersionId --output text)
 
@@ -21,5 +20,6 @@ S3_OBJVER=$(aws s3api head-object --bucket $S3_BUCKET --key $S3_KEY --query Vers
 CONTAINER_ID=$(docker run -d glassechidna/sshello)
 CONTAINER_IP=$(docker inspect $CONTAINER_ID --format '{{ .NetworkSettings.IPAddress }}')
 
-go run main.go ssh exec -- -o StrictHostKeyChecking=no -p 2222 travis@$CONTAINER_IP | tee out.log
+docker run -it -v $(pwd):$(pwd) -w $(pwd) rastasheep/ubuntu-sshd:16.04 ./lastkeypair_linux_amd64 ssh exec -- -o StrictHostKeyChecking=no -p 2222 travis@$CONTAINER_IP | tee out.log
+#go run main.go ssh exec -- -o StrictHostKeyChecking=no -p 2222 travis@$CONTAINER_IP | tee out.log
 diff out.log ci/expected-output.txt
