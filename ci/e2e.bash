d6f79c7d21da29ff6d634bd8d2ff92d233410255
Aidan Steele <aidan.steele@glassechidna.com.au>
2017-06-30 18:23:36 +1000

    quiet

--- ci/e2e.bash
+++ ci/e2e.bash
@@ -20,6 +20,6 @@ S3_OBJVER=$(aws s3api head-object --bucket $S3_BUCKET --key $S3_KEY --query Vers
 CONTAINER_ID=$(docker run -d glassechidna/sshello)
 CONTAINER_IP=$(docker inspect $CONTAINER_ID --format '{{ .NetworkSettings.IPAddress }}')
 
-docker run -it -v $(pwd):$(pwd) -w $(pwd) -e AWS_ACCESS_KEY_ID -e AWS_REGION -e AWS_SECRET_ACCESS_KEY rastasheep/ubuntu-sshd:16.04 ./lastkeypair_linux_amd64 ssh exec -- -o StrictHostKeyChecking=no -p 2222 travis@$CONTAINER_IP | tee out.log
+docker run -it -v $(pwd):$(pwd) -w $(pwd) -e AWS_ACCESS_KEY_ID -e AWS_REGION -e AWS_SECRET_ACCESS_KEY rastasheep/ubuntu-sshd:16.04 ./lastkeypair_linux_amd64 ssh exec -- -o StrictHostKeyChecking=no -o LogLevel=QUIET -p 2222 travis@$CONTAINER_IP | tee out.log
 #go run main.go ssh exec -- -o StrictHostKeyChecking=no -p 2222 travis@$CONTAINER_IP | tee out.log
 diff out.log ci/expected-output.txt
