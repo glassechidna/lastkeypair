4b439b58c68c4da3380f37620f95cfb0c9615a39
Aidan Steele <aidan.steele@glassechidna.com.au>
2017-06-30 10:19:04 +1000

    more CI

--- ci/e2e.bash
+++ ci/e2e.bash
@@ -10,7 +10,7 @@ make # build lambda package
 aws s3 cp handler.zip s3://$S3_BUCKET/$S3_KEY
 S3_OBJVER=$(aws s3api head-object --bucket $S3_BUCKET --key $S3_KEY --query VersionId --output text)
 
-./stackit \
+./stackit up \
   --stack-name lkp-lambda-test \
   --template cfn.yml \
   --previous-param-value PstoreCAKeyBytesName
@@ -18,5 +18,8 @@ S3_OBJVER=$(aws s3api head-object --bucket $S3_BUCKET --key $S3_KEY --query Vers
   --previous-param-value S3Key
   --param-value S3ObjectVersion=$S3_OBJVER
 
-go run main.go ssh exec | tee out.log
+CONTAINER_ID=$(docker run -d glassechidna/sshello)
+CONTAINER_IP=$(docker inspect $CONTAINER_ID --format '{{ .NetworkSettings.IPAddress }}')
+
+go run main.go ssh exec -- travis@$CONTAINER_IP | tee out.log
 diff out.log ci/expected-output.txt
