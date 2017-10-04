ff540b4d97990d5edd0c0bb9443a23ec63015ed2
Aidan Steele <aidan.steele@glassechidna.com.au>
2017-10-05 09:35:26 +1100

    CI: update to upx 3.94 and compress mac binaries

--- ci/setup_env.bash
+++ ci/setup_env.bash
@@ -3,7 +3,7 @@ set -euxo pipefail
 
 go get github.com/mitchellh/gox
 
-curl -z dl/upx.txz -o dl/upx.txz -L https://github.com/upx/upx/releases/download/v3.93/upx-3.93-amd64_linux.tar.xz
+curl -z dl/upx.txz -o dl/upx.txz -L https://github.com/upx/upx/releases/download/v3.94/upx-3.94-amd64_linux.tar.xz
 tar -xvf dl/upx.txz
 
 curl -z dl/glide.tgz -o dl/glide.tgz -L https://github.com/Masterminds/glide/releases/download/v0.12.3/glide-v0.12.3-linux-amd64.tar.gz
