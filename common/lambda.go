1c7509d038ebaa23f6cd2522918fce04c70df33e
Aidan Steele <aidan.steele@glassechidna.com.au>
2017-10-04 14:58:15 +1100

    Extracted authoriser lambda code into its own file

--- common/lambda.go
+++ common/lambda.go
@@ -16,7 +16,6 @@ import (
 	"fmt"
 	"log"
 	"golang.org/x/crypto/ssh"
-	"github.com/aws/aws-sdk-go/service/lambda"
 )
 
 type UserCertReqJson struct {
@@ -46,37 +45,6 @@ type Jumpbox struct {
 
 type HostCertRespJson struct {
 	SignedHostPublicKey string
-}
-
-type AuthorizationLambdaIdentity struct {
-	Name    *string `json:",omitempty"`
-	Id      string
-	Account string
-	Type    string
-}
-
-type AuthorizationLambdaVoucher struct {
-	Name    *string `json:",omitempty"`
-	Id      string
-	Account string
-	Type    string
-	Vouchee	string
-	Context string
-}
-
-type AuthorizationLambdaRequest struct {
-	From AuthorizationLambdaIdentity
-	RemoteInstanceArn string
-	Vouchers []AuthorizationLambdaVoucher `json:",omitempty"`
-}
-
-type AuthorizationLambdaResponse struct {
-	Authorized bool
-	Jumpbox *Jumpbox `json:",omitempty"`
-	CertificateOptions struct {
-		ForceCommand *string `json:",omitempty"`
-		SourceAddress *string `json:",omitempty"`
-	}
 }
 
 type LambdaConfig struct {
@@ -277,59 +245,4 @@ func DoUserCertReq(req UserCertReqJson, config LambdaConfig) (*UserCertRespJson,
 	}
 
 	return &resp, nil
-}
-
-func DoAuthorizationLambda(userReq UserCertReqJson, config LambdaConfig) (*AuthorizationLambdaResponse, error) {
-	if len(config.AuthorizationLambda) == 0 {
-		return &AuthorizationLambdaResponse{Authorized: true}, nil
-	}
-
-	client := lambda.New(LambdaAwsSession())
-
-	p := userReq.Token.Params
-	req := AuthorizationLambdaRequest{
-		From: AuthorizationLambdaIdentity{
-			Name: &p.FromName,
-			Id: p.FromId,
-			Account: p.FromAccount,
-			Type: p.Type,
-		},
-		RemoteInstanceArn: p.RemoteInstanceArn,
-	}
-
-	for _, v := range p.Vouchers {
-		vp := v.Params
-		voucher := AuthorizationLambdaVoucher{
-			Name: &vp.FromName,
-			Id: vp.FromId,
-			Account: vp.FromAccount,
-			Type: vp.Type,
-			Vouchee: vp.Vouchee,
-			Context: vp.Context,
-		}
-		req.Vouchers = append(req.Vouchers, voucher)
-	}
-
-	encoded, err := json.Marshal(&req)
-	if err != nil {
-		return nil, errors.Wrap(err, "encoding authorisation lambda request")
-	}
-
-	input := &lambda.InvokeInput{
-		FunctionName: &config.AuthorizationLambda,
-		Payload: encoded,
-	}
-
-	resp, err := client.Invoke(input)
-	if err != nil {
-		return nil, errors.Wrap(err, "executing authorisation lambda")
-	}
-
-	authResp := AuthorizationLambdaResponse{}
-	err = json.Unmarshal(resp.Payload, &authResp)
-	if err != nil {
-		return nil, errors.Wrap(err, "decoding auth lambda response")
-	}
-
-	return &authResp, nil
 }
