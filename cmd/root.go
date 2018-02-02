d8d41ad1c53e9cc89ab339494697b60b033987d1
Aidan Steele <aidan.steele@glassechidna.com.au>
2018-02-02 13:51:18 +1100

    CLI root command docs

--- cmd/root.go
+++ cmd/root.go
@@ -12,13 +12,14 @@ var cfgFile string
 
 var RootCmd = &cobra.Command{
 	Use:   "lastkeypair",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "A serverless SSH certificate authority to control access to machines using IAM and Lambda",
+	Long: `
+lastkeypair is a CLI tool and a Lambda function to control remote access to 
+instances through SSH certificates. User certificates are created on-demand and
+are specific to a user and target server. Host certificates are also created
+at instance launch time and allow for host validation without needing host key
+validation prompts.
+`,
 }
 
 func Execute() {
@@ -31,7 +32,7 @@ func Execute() {
 func init() {
 	cobra.OnInitialize(initConfig)
 
-	RootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.lastkeypair.yaml)")
+	RootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.lkp/config.yml)")
 	RootCmd.PersistentFlags().StringP("profile", "p", "", "Name of profile in ~/.aws/config to use")
 }
 
