baf51bf03a9e939603764510e829ea0648e2b338
Aidan Steele <aidan.steele@glassechidna.com.au>
2018-07-28 12:31:36 +1000

    Switch from glide to dep for package mgmt (closes #45)

--- ci/setup_env.bash
+++ ci/setup_env.bash
@@ -6,9 +6,6 @@ go get github.com/mitchellh/gox
 curl -z dl/upx.txz -o dl/upx.txz -L https://github.com/upx/upx/releases/download/v3.94/upx-3.94-amd64_linux.tar.xz
 tar -xvf dl/upx.txz
 
-curl -z dl/glide.tgz -o dl/glide.tgz -L https://github.com/Masterminds/glide/releases/download/v0.12.3/glide-v0.12.3-linux-amd64.tar.gz
-tar -xvf dl/glide.tgz
-
 curl -z dl/stackit -o dl/stackit -L https://github.com/glassechidna/stackit/releases/download/0.0.9/stackit_linux_amd64
 chmod +x dl/stackit
 
